9a2eb9047d6236267a6e4c5994ad7b435a1b5f7c
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    a73xq: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     sensors.a73xq
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # UDFPS
 $(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x20000000u)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
