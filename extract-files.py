e6cb722e99f8fd63b3f53219c6edc0a2315f3551
Bush-cat <bushccat@gmail.com>
2026-02-17 13:09:49 +0000

    a73xq: update blobs from A736BXXSAGZA1
    Change-Id: Ied5402951f7118da8dd355be65e0f4780aa88379

--- extract-files.py
+++ extract-files.py
@@ -26,11 +26,11 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .sig_replace(
-            '80 06 40 F9 E1 78 00 94 80 01 00 34 82 DB FF D0 A3 DD FF F0 A5 DC FF 90 E6 03 00 2A',
+            '80 06 40 F9 E5 78 00 94 80 01 00 34 82 DB FF D0 A3 DD FF F0 A5 DC FF 90 E6 03 00 2A',
             '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5'
         )
         .sig_replace(
-            '42 0C 0E 91 63 EC 0F 91 A5 20 1A 91 A4 11 80 52 E0 03 1F 2A 21 00 80 52 8D FE FF 97 80 06 40 F9',
+            '42 DC 0F 91 63 74 13 91 A5 A8 1C 91 A4 11 80 52 E0 03 1F 2A 21 00 80 52 8D FE FF 97 80 06 40 F9',
             '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 80 06 40 F9'
         ),
     'vendor/lib64/nfc_nci_nxpsn.so': blob_fixup()
